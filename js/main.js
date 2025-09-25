6be89f11fea3174bce2eb927c4203915dc312acd
albert <alberix252@gmail.com>
2025-09-25 14:00:26 -0500

    Se creo la funcion DOMContentLoaded  con el JSON.parse para convertir texto en formato JSON a uno formato JS

--- js/main.js
+++ js/main.js
@@ -1,44 +1,74 @@
 //creamos las constantes globales
-const form = document.getElementById('formulario');
-const input = document.getElementById('nombre');
-const saludo = document.getElementById('saludo');
-const btnBorrar = document.getElementById('borrar');
+const form = document.getElementById('formulario-curso');
+const inputCurso = document.getElementById('nombre-curso');
+const inputProfesor = document.getElementById('nombre-profesor');
+const inputPrecio = document.getElementById('precio-curso');
+const inputCiudad =document.getElementById('ciudad-curso');
+const inputCupo = document.getElementById('cupo-curso');
+const mensajeCurso = document.getElementById('mensaje-curso');
+const btnBorrarCurso = document.getElementById('borrar');
+
 
 
 //aca evitamos q la pagina se recargue y verificamos que hayan ingresado un nombre
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const nombre = input.value.trim();
+    const curso = inputCurso.value.trim();
+    const profesor = inputProfesor.value.trim();
+    const precio = inputPrecio.value.trim();
+    const ciudad = inputCiudad.value.trim();
+    const cupo = inputCupo.value.trim();
+    
 
-    if (nombre == "") {
-        alert('ingrese su nombre');
+    if (curso == "" || profesor == "" || precio == "" || ciudad == "" || cupo == "" ) {
+        alert('ingrese todos los datos solicitados');
         return;
     }
-    //aca aparece el nombre que ingresamos  en el navegador
-    localStorage.setItem('usuario', nombre);
-    saludo.textContent = 'Bienvenido   ' + nombre;
-    input.value = "";
-});
 
-
-
-//aca aparece el usuario nuevo que se eguardo
-document.addEventListener('DOMContentLoaded', () => {
-    const usuarioGuardado = localStorage.getItem('usuario');
-
-    if (usuarioGuardado) {
-        saludo.textContent = 'Bienvenido de nuevo  ' + usuarioGuardado;
+    //en este ejercico se construye un  objeto con caracteristicas sus diferentes claves y valores
+    const nuevoCurso = {
+        nombre: curso,
+        profesor: profesor,
+        precio: precio,
+        ciudad: ciudad,
+        cupo: cupo
     }
+
+    localStorage.setItem('curso', JSON.stringify(nuevoCurso));
+    form.reset();
 });
 
+//aca aparece el curso nuevo que se eguardo
+document.addEventListener('DOMContentLoaded', () => {
+        const cursoCreado = localStorage.getItem('curso');
+
+        if (cursoCreado) {
+            const objetoCurso = JSON.parse(cursoCreado)//sirve para convertir un texto en formato JSON a un objeto de JavaScript. 
+  
+            mensajeCurso.textContent = 'curso: ' + objetoCurso.nombre + 'profesor: ' + objetoCurso.profesor + 'precio: ' + objetoCurso.precio + 'ciudad: ' + objetoCurso.ciudad + 'cupo: ' + objetoCurso.cupo;
+        }
+      
+    });
 
 
-//aca se elimina el nombre que el usuario coloco
-btnBorrar.addEventListener('click', () => {
-    localStorage.removeItem('usuario'); // elimina el nombre de ususario
-    saludo.textContent = 'Bienvenido ususario Indefinido';
-});
+
+
+
+
+
+
+
+
+
+
+
+
+// //aca se elimina el nombre que el usuario coloco
+// btnBorrar.addEventListener('click', () => {
+//     localStorage.removeItem('curso'); // elimina el nombre de ususario
+//     saludo.textContent = 'Bienvenido ususario Indefinido';
+// });
 
 
 
